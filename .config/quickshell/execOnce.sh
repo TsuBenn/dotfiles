eb76fa3996c8d1a2df2e776fc05340fae5e0a5f7
TsuBenn <TsuBenn@github.com>
2025-11-17 13:22:03 +0700

    quickshell: added some debugging logs

--- .config/quickshell/execOnce.sh
+++ .config/quickshell/execOnce.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo ""
+echo "Executing!"
+
 hyprctl keyword layerrule blur, quickshell
 hyprctl keyword unbind SUPER,ESCAPE
 hyprctl keyword bind SUPER,ESCAPE,exec, qs ipc call homepanel toggle
+
+echo "Executed!"
