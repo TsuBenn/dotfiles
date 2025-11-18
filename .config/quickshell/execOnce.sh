674606cf90f7d0614df7dd8f5f8b6c8f71f62a6a
TsuBenn <TsuBenn@github.com>
2025-11-18 23:41:35 +0700

    quickshell: Dependencies checker

--- .config/quickshell/execOnce.sh
+++ .config/quickshell/execOnce.sh
@@ -3,6 +3,26 @@
 echo ""
 echo "Executing!"
 
+TARGET_PACKAGES=(
+    "playerctl"
+    "github-cli"
+)
+
+echo ""
+echo -e "--- Checking Required Dependencies (${#TARGET_PACKAGES[@]} total) ---"
+echo ""
+for pkg in "${TARGET_PACKAGES[@]}"; do
+if pacman -Qi "$pkg" &> /dev/null; then
+    echo "[${pkg}] is installed."
+else
+    echo "[${pkg}] is not installed."
+fi
+done
+echo ""
+echo "--- Finished Checking Prerequisite...---"
+echo ""
+
+
 hyprctl keyword layerrule blur, quickshell
 hyprctl keyword unbind SUPER,ESCAPE
 hyprctl keyword bind SUPER,ESCAPE,exec, qs ipc call homepanel toggle
