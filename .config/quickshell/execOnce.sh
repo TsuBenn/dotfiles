296c6df25612b47bf63449cb7e21bd253c1b17b0
TsuBenn <subenn2007@gmail.com>
2026-01-09 00:16:36 +0700

    Quickshell: swiping 2 fingers down will open the Homepanel on laptop

--- .config/quickshell/execOnce.sh
+++ .config/quickshell/execOnce.sh
@@ -6,5 +6,6 @@ echo "Executing!"
 #hyprctl keyword layerrule blur on, match:namespace quickshell
 hyprctl keyword unbind SUPER,ESCAPE
 hyprctl keyword bind SUPER,ESCAPE,exec, qs ipc call homepanel toggle
+hyprctl keyword gesture 3, down, dispatcher, exec, qs ipc call homepanel toggle
 
 echo "Executed!"
